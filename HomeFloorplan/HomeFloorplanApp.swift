39072f1a4e37ae362ae7b99847280dbaf285bd32
Maurizio Cinti <override1978@gmail.com>
2026-05-17 21:58:45 +0200

    Initial Commit

---
+++ HomeFloorplan/HomeFloorplanApp.swift
@@ -0,0 +1,32 @@
+//
+//  HomeFloorplanApp.swift
+//  HomeFloorplan
+//
+//  Created by Maurizio Cinti on 17/05/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct HomeFloorplanApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
