e2cf3ad1dbdffd0a78649b3c14ab3746a16830ef
Maurizio Cinti <override1978@gmail.com>
2026-05-20 20:43:50 +0200

    Messaggio suggerito: "Sidebar + galleria floorplan + edit unico (immagine+nome) + swipe actions

---
+++ HomeFloorplan/Views/EditFloorplanSheet.swift
@@ -0,0 +1,18 @@
+//
+//  EditFloorplanSheet.swift
+//  HomeFloorplan
+//
+//  Created by Maurizio Cinti on 19/05/26.
+//
+
+import SwiftUI
+
+struct EditFloorplanSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditFloorplanSheet()
+}
---
+++ HomeFloorplan/Views/RenameFloorplanSheet.swift
@@ -0,0 +1,18 @@
+//
+//  RenameFloorplanSheet.swift
+//  HomeFloorplan
+//
+//  Created by Maurizio Cinti on 19/05/26.
+//
+
+import SwiftUI
+
+struct RenameFloorplanSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RenameFloorplanSheet()
+}
